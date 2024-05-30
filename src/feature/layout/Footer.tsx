f9f6f74d3b201e9eeb24adb726f57b3d6cca71e8
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-05-30 19:11:00 +0000

    NextAuth authentification

--- src/feature/layout/Footer.tsx
+++ src/feature/layout/Footer.tsx
@@ -5,27 +5,25 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="border-t border-t-accent">
-      <div className="py-2 flex justify-between container gap-1 fixed bottom-0 left-0 right-0 bg-background max-w-lg m-auto">
-        <Link
-          href={"/"}
-          className={clsx(buttonVariants({ variant: "ghost" }), "flex-1")}
-        >
-          <Home size={16} />
-        </Link>
-        <Link
-          href={"/"}
-          className={clsx(buttonVariants({ variant: "ghost" }), "flex-1")}
-        >
-          <PenSquare size={16} />
-        </Link>
-        <Link
-          href={"/"}
-          className={clsx(buttonVariants({ variant: "ghost" }), "flex-1")}
-        >
-          <User size={16} />
-        </Link>
-      </div>
+    <div className="py-2 flex justify-between container gap-1 fixed bottom-0 left-0 right-0 bg-background max-w-lg m-auto border-t border-t-accent">
+      <Link
+        href={"/"}
+        className={clsx(buttonVariants({ variant: "ghost" }), "flex-1")}
+      >
+        <Home size={16} />
+      </Link>
+      <Link
+        href={"/whrite"}
+        className={clsx(buttonVariants({ variant: "ghost" }), "flex-1")}
+      >
+        <PenSquare size={16} />
+      </Link>
+      <Link
+        href={"/profile"}
+        className={clsx(buttonVariants({ variant: "ghost" }), "flex-1")}
+      >
+        <User size={16} />
+      </Link>
     </div>
   );
 };
