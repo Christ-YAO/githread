e2bc25f6ab1134eac049a40f09c4a7b697254781
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-05-31 19:33:24 +0000

    fixed login issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,13 @@
 import GithubProvider from "next-auth/providers/github"
 import { env } from "./env";
 import { AuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "./prisma";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { prisma } from "./prisma";
 
 
 export const authOptions: AuthOptions = {
     // Configure one or more authentication providers
-    adapter: PrismaAdapter(prisma),
+    // adapter: PrismaAdapter(prisma),
     providers: [
       GithubProvider({
         clientId: env.GITHUB_ID,
