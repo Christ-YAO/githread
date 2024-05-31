1fb4e84e2f443df1fcf713eb7bfa60ed67bb98ad
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-05-31 19:37:35 +0000

    fixed Header to the top

--- src/feature/layout/Header.tsx
+++ src/feature/layout/Header.tsx
@@ -3,7 +3,7 @@ import LoginButton from "./auth/LoginButton";
 
 export const Header = async () => {
   return (
-    <header className="border-b border-b-accent">
+    <header className="border-b border-b-accent fixed top-0 bg-background w-full">
       <div className="container flex items-center py-2 max-w-lg m-auto gap-1">
         <h2 className="text-2xl font-bold mr-auto">Githread</h2>
         <LoginButton />
