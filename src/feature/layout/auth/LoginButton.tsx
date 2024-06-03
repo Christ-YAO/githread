6a7526dc40bac5bb7fd022c9b2c66d7a8302cf2d
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-03 17:00:22 +0000

    add dropdown button

--- src/feature/layout/auth/LoginButton.tsx
+++ src/feature/layout/auth/LoginButton.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Loader } from "@/components/ui/Loader";
 import { LogIn } from "lucide-react";
 import { signIn } from "next-auth/react";
+import { useTransition } from "react";
 
 export default function LoginButton() {
+  const [isPending, startTransition] = useTransition();
   return (
     <Button
       onClick={() => {
-        signIn();
+        startTransition(() => signIn());
       }}
     >
-      <LogIn className="mr-2 h-4 w-4" />
+      {isPending ? (
+        <Loader className="mr-2 h-4 w-4" />
+      ) : (
+        <LogIn className="mr-2 h-4 w-4" />
+      )}
       Login
     </Button>
   );
