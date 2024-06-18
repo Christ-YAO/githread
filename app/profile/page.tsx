703ddc4061d4a91ee5ae4c3eb498c948503ecf32
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-18 19:32:49 +0000

    Started edit profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,41 @@
-export default function ProfilPage() {
-    return (
-        <div>Profile</div>
-    );
+import { getAuthSession } from "@/lib/auth";
+import { getUserProfile } from "@/query/user.query";
+import Profile from "../users/[userId]/Profile";
+import Post from "@/components/features/post/Post";
+import { notFound } from "next/navigation";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export default async function ProfilPage() {
+  const session = await getAuthSession();
+
+  if (!session?.user.id) {
+    notFound();
+  }
+
+  const user = await getUserProfile(session.user.id);
+
+  if (!user) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <Profile user={user}>
+        <form className="mt-4">
+          <Link
+            href={"profile/edit"}
+            className={buttonVariants({ variant: "outline" })}
+          >
+            Edit Profile
+          </Link>
+        </form>
+      </Profile>
+      <div className="divide-y divide-accent border-t border-accent mt-4">
+        {user.posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
