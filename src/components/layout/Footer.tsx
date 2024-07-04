0b487a268679f6cc825014fef0d05726f0166921
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-04 19:02:21 +0000

    change footer role

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,43 +1,52 @@
-import Link from 'next/link';
-import clsx from 'clsx';
-import { buttonVariants } from '../ui/button';
-import { Home, PenSquare, User } from 'lucide-react';
+"use client";
+
+import Link from "next/link";
+import clsx from "clsx";
+import { buttonVariants } from "../ui/button";
+import { Home, PenSquare, User } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export const Footer = () => {
+  const pathname = usePathname();
   return (
-    <div className="py-2 flex justify-between container gap-1 fixed bottom-0 left-0 right-0 bg-background max-w-lg m-auto border-t border-accent">
+    <div className="py-2 flex-1 flex justify-center items-center container gap-1 max-w-md">
       <Link
         href="/"
         className={clsx(
           buttonVariants({
-            variant: 'ghost',
+            variant: "ghost",
           }),
-          'flex-1'
+          "flex-1",
+          { "bg-accent": pathname === "/" }
         )}
       >
-        <Home size={16} />
+        <Home size={20} />
       </Link>
       <Link
         href="/write"
         className={clsx(
           buttonVariants({
-            variant: 'ghost',
+            variant: "ghost",
           }),
-          'flex-1'
+          "flex-1",
+          { "bg-accent": pathname?.includes("/write") }
         )}
       >
-        <PenSquare size={16} />
+        <PenSquare size={20} />
       </Link>
       <Link
         href="/profile"
         className={clsx(
           buttonVariants({
-            variant: 'ghost',
+            variant: "ghost",
           }),
-          'flex-1'
+          "flex-1",
+          {
+            "bg-accent": pathname?.includes("/profile"),
+          }
         )}
       >
-        <User size={16} />
+        <User size={20} />
       </Link>
     </div>
   );
