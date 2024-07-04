2a7c4985fa6d4f300bb58d36de513865bd868ebb
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-04 20:02:23 +0000

    Add report dropdown

--- src/components/features/post/PostLayout.tsx
+++ src/components/features/post/PostLayout.tsx
@@ -3,8 +3,16 @@ import clsx from "clsx";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { formatDate } from "@/lib/date";
-import { MoreHorizontal } from "lucide-react";
+import { LogOut, MoreHorizontal, OctagonAlert } from "lucide-react";
 import { PostHome } from "@/query/post.query";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 type PostLayoutProps = PropsWithChildren<{
   user: PostHome["user"];
@@ -18,7 +26,7 @@ export default function PostLayout({
   user,
   createdAdt,
   postId,
-  children
+  children,
 }: PostLayoutProps) {
   return (
     <div className={clsx("flex w-full flex-row items-start p-4", className)}>
@@ -31,21 +39,34 @@ export default function PostLayout({
         </AvatarFallback>
       </Avatar>
       <div className="ml-4 flex w-full flex-col gap-2">
-        <Link href={`/users/${user.id}`}>
-          <div className="flex flex-row items-center gap-2">
-            <p className="text-sm text-card-foreground mr-auto">
-              {user.username}
-            </p>
-            {createdAdt ? (
-              <p className="text-sm text-muted-foreground">
-                {formatDate(createdAdt)}
+        <div>
+          <div className="flex flex-row items-center justify-between gap-2">
+            <Link href={`/users/${user.id}`} className="flex gap-2">
+              <p className="text-sm text-card-foreground mr-auto">
+                {user.username}
               </p>
-            ) : null}
-            <button>
-              <MoreHorizontal size={20} />
-            </button>
+              {createdAdt ? (
+                <p className="text-sm text-muted-foreground font-light">
+                  {formatDate(createdAdt)}
+                </p>
+              ) : null}
+            </Link>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant={"ghost"} className="rounded-3xl p-2">
+                  <MoreHorizontal size={20} strokeWidth={"1.5px"} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem className="text-red-600 flex items-center justify-between font-bold cursor-pointer">
+                  report
+                  <OctagonAlert size={16} className="ml-2" />
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-        </Link>
+        </div>
         {children}
       </div>
     </div>
