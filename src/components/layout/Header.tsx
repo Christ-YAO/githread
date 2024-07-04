77529bc5b12dbcfc8e0e464b75ce1476edd71bdc
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-04 19:02:48 +0000

    inmport footer in the header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,18 +4,21 @@ import { Typography } from "../ui/Typography";
 import { ThemeToggle } from "../theme/ThemeToggle";
 import { SiteConfig } from "@/lib/site-config";
 import { AuthButton } from "../features/auth/AuthButton";
+import { Footer } from "./Footer";
 
 export function Header() {
   return (
-    <header className="border-b border-b-accent fixed z-20 top-0 bg-background w-full">
-      <div className="container flex items-center py-2 max-w-lg m-auto gap-1">
+    <header className="border-b border-b-accent fixed z-20 top-0 w-full backdrop-blur-xl">
+      <div className="container flex items-center justify-between py-2 max-w-5xl m-auto gap-1">
         <div className="flex gap-2 items-center">
           <Typography variant="h3" as={Link} href="/">
             {SiteConfig.title}
           </Typography>
         </div>
-
-        <div className="flex flex-1 items-center justify-end space-x-4">
+        {/* Links */}
+        <Footer />
+        {/* Links */}
+        <div className="flex items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <AuthButton />
             <ThemeToggle />
