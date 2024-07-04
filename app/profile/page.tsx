fa716de7fb59fee073b5642708ba7390f84a8667
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-04 18:59:16 +0000

    Add error section when isn't logged in

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,18 +5,30 @@ import Post from "@/components/features/post/Post";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { AuthButton } from "@/components/features/auth/AuthButton";
 
 export default async function ProfilPage() {
   const session = await getAuthSession();
 
   if (!session?.user.id) {
-    notFound();
+    return (
+      <div className="grid place-content-center h-[80vh] gap-4 px-4">
+        <div className="w-full grid">
+          <div className="flex items-center justify-center text-sm sm:text-nowrap">
+            <h5 className="text-lg font-semibold capitalize">Not Logged In</h5>{" "}
+            <div className="h-10 w-[0.5px] bg-accent mx-4"></div>You must be
+            logged in before you can view posts !
+          </div>
+        </div>
+        <AuthButton />
+      </div>
+    );
   }
 
   const user = await getUserProfile(session.user.id);
 
   if (!user) {
-    notFound();
+    throw new Error(`User not found`);
   }
 
   return (
