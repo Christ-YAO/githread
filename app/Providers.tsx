3de04630dd418a5788ef3ca70725335fd8f5a793
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-10 17:25:05 +0000

    default mode dark

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import { ToastProvider } from '@/components/ui/toast';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { SessionProvider } from 'next-auth/react';
-import { ThemeProvider } from 'next-themes';
-import { PropsWithChildren } from 'react';
+import { ToastProvider } from "@/components/ui/toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { PropsWithChildren } from "react";
 
 const queryClient = new QueryClient();
 
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <SessionProvider>
           <ToastProvider>
             <QueryClientProvider client={queryClient}>
