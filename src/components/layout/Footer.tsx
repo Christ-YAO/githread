c23bb66455092fb4e186551b49383169e0fea2e8
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-08-23 17:52:51 +0000

    Maked footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,55 +3,57 @@
 import Link from "next/link";
 import clsx from "clsx";
 import { buttonVariants } from "../ui/button";
-import { Home, PenSquare, User } from "lucide-react";
 import { usePathname } from "next/navigation";
-import GoBack from "./GoBack";
+import { Home, PenSquare, User } from "lucide-react";
 
-export const Footer = () => {
-  const pathname = usePathname();
-  return (
-    <div className="py-2 flex-1 hidden  sm:flex justify-center items-center container gap-1 max-w-xs md:max-w-sm">
-      <div className="hidden sm:block">
-        <GoBack />
-      </div>
-      <Link
-        href="/"
-        className={clsx(
-          buttonVariants({
-            variant: "ghost",
-          }),
-          "flex-1",
-          { "bg-accent": pathname === "/" }
-        )}
-      >
-        <Home size={20} />
-      </Link>
-      <Link
-        href="/write"
-        className={clsx(
-          buttonVariants({
-            variant: "ghost",
-          }),
-          "flex-1",
-          { "bg-accent": pathname?.includes("/write") }
-        )}
-      >
-        <PenSquare size={20} />
-      </Link>
-      <Link
-        href="/profile"
-        className={clsx(
-          buttonVariants({
-            variant: "ghost",
-          }),
-          "flex-1",
-          {
-            "bg-accent": pathname?.includes("/profile"),
-          }
-        )}
-      >
-        <User size={20} />
-      </Link>
-    </div>
-  );
-};
+export function Footer() {
+    const pathname = usePathname();
+    return (
+        <header className="fixed z-20 bottom-0 w-full bg-[#000000c8] backdrop-blur-xl md:hidden">
+            <div className="container flex items-center justify-between py-2 max-w-5xl m-auto gap-1">
+                {/* Links */}
+                <div className="py-1 flex-1 flex justify-evenly items-center container gap-1 ">
+                    <Link
+                        href="/"
+                        className={clsx(
+                            buttonVariants({
+                                variant: "ghost",
+                            }),
+                            "flex-1",
+                            { "bg-accent/65": pathname === "/" }
+                        )}
+                    >
+                        <Home size={20} />
+                    </Link>
+                    <Link
+                        href="/write"
+                        className={clsx(
+                            buttonVariants({
+                                variant: "ghost",
+                            }),
+                            "flex-1",
+                            { "bg-accent/65": pathname?.includes("/write") }
+                        )}
+                    >
+                        <PenSquare size={20} />
+                    </Link>
+                    <Link
+                        href="/profile"
+                        className={clsx(
+                            buttonVariants({
+                                variant: "ghost",
+                            }),
+                            "flex-1",
+                            {
+                                "bg-accent/65": pathname?.includes("/profile"),
+                            }
+                        )}
+                    >
+                        <User size={20} />
+                    </Link>
+                </div>
+                {/* Links */}
+            </div>
+        </header>
+    );
+}
