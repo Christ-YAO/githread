1a9b5e90ad60fc0d9d0a6c61b894dd37ee0861ea
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-08-23 16:22:53 +0000

    Prisma added media images

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,65 +5,79 @@ import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 const main = async () => {
-    const users = [];
-    for (let i = 0; i < 10; i++) {
-        const user = {
-            username: faker.internet.userName(),
-            image: faker.image.avatar(),
-            name: faker.person.firstName(),
-            bio: faker.lorem.paragraph(),
-            link: faker.internet.url(),
-            email: faker.internet.email()
-        } satisfies Prisma.UserCreateInput;
-        const dbUser = await prisma.user.create({data: user});
-        users.push(dbUser);
+  const users = [];
+  for (let i = 0; i < 10; i++) {
+    const user = {
+      username: faker.internet.userName(),
+      image: faker.image.avatar(),
+      name: faker.person.firstName(),
+      bio: faker.lorem.paragraph(),
+      link: faker.internet.url(),
+      email: faker.internet.email(),
+    } satisfies Prisma.UserCreateInput;
+    const dbUser = await prisma.user.create({ data: user });
+    users.push(dbUser);
+  }
+
+  const posts = [];
+  for (let i = 0; i < 100; i++) {
+    const randomUserIndex = faker.number.int({
+      min: 0,
+      max: users.length - 1,
+    });
+
+    const randomWorldCount = faker.number.int({
+      min: 5,
+      max: 12,
+    });
+
+    const randomMediaCount = faker.number.int({
+      min: 0,
+      max: 5,
+    });
+
+    // Génération des médias
+    const mediaArray: string[] = [];
+    for (let j = 0; j < randomMediaCount; j++) {
+      mediaArray.push(faker.image.url());
     }
 
-    const posts = []
-    for (let i = 0; i < 100; i++) {
-        const randomUserIndex = faker.number.int({
-            min: 0,
-            max: users.length - 1
-        })
+    const post = {
+      content: faker.lorem.sentence(randomWorldCount),
+      userId: users[randomUserIndex].id,
+      media: mediaArray,
+    } satisfies Prisma.PostUncheckedCreateInput;
 
-        const randomWorldCount = faker.number.int({
-            min: 5,
-            max: 12
-        })
+    const p = await prisma.post.create({ data: post });
+    posts.push(p);
+  }
 
-        const post = {
-            content: faker.lorem.sentence(randomWorldCount),
-            userId: users[randomUserIndex].id,
-        } satisfies Prisma.PostUncheckedCreateInput;
+  for (let i = 0; i < 200; i++) {
+    const randomUserIndex = faker.number.int({
+      min: 0,
+      max: users.length - 1,
+    });
 
-        const p = await prisma.post.create({data: post});
-        posts.push(p);
-    }
+    const randomPostIndex = faker.number.int({
+      min: 0,
+      max: posts.length - 1,
+    });
 
-    for (let i = 0; i < 200; i++) {
-        const randomUserIndex = faker.number.int({
-            min: 0,
-            max: users.length - 1
-        })
+    const like = {
+      postId: posts[randomPostIndex].id,
+      userId: users[randomUserIndex].id,
+    } satisfies Prisma.LikeUncheckedCreateInput;
 
-        const randomPostIndex = faker.number.int({
-            min: 0,
-            max: posts.length - 1
-        })
+    await prisma.like.create({ data: like });
+  }
+};
 
-        const like = {
-            postId: posts[randomPostIndex].id,
-            userId: users[randomUserIndex].id,
-        } satisfies Prisma.LikeUncheckedCreateInput;
-
-        await prisma.like.create({data: like})
-    }
-}
-
-main().then(async () => {
+main()
+  .then(async () => {
     await prisma.$disconnect();
-}).catch(async e => {
+  })
+  .catch(async (e) => {
     console.error(e);
     await prisma.$disconnect();
     process.exit(1);
-})
+  });
