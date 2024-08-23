64792967b0ae35834beb495d1f71dfe42e137b96
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-08-23 16:25:37 +0000

    zoom image and show post medias

--- src/components/features/post/Post.tsx
+++ src/components/features/post/Post.tsx
@@ -1,21 +1,78 @@
+"use client";
+
 import Link from "next/link";
 import { PostHome } from "../../../query/post.query";
 import PostLayout from "./PostLayout";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { Heart, MessageCircle } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { MessageCircle } from "lucide-react";
 import LikeButton from "./LikeButton";
 import { cn } from "@/lib/utils";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { useEffect, useState } from "react";
+import useDetectScroll, { Direction } from "@smakss/react-scroll-direction";
 
 type PostProps = {
   post: PostHome;
 };
 
 export default function Post({ post }: PostProps) {
+
+  // State variable for managing zoomed image
+  const [zoomedImage, setZoomedImage] = useState<string | null>(null);
+  // Function to open zoomed image
+  const openZoomedImage = (imageUrl: string | null) => {
+    setZoomedImage(imageUrl);
+  };
+  // Function to close zoomed image
+  const closeZoomedImage = () => {
+    setZoomedImage(null);
+  };
+  
+
+  const { scrollDir, scrollPosition } = useDetectScroll();
+
+  useEffect(() => {
+    if (scrollDir === Direction.Up || scrollDir === Direction.Down) {
+      setZoomedImage(null);
+    }
+  }, [scrollPosition]);
+
   return (
-    <PostLayout user={post.user} postId={post.id} createdAdt={post.createdAt}>
+    <PostLayout user={post.user} postId={post.id} createdAdt={post.createdAt} className="group">
       <Link href={`/posts/${post.id}`} className="text-sm text-foreground">
         {post.content}
       </Link>
+      {post.media && post.media?.length > 1 ? (
+        <Carousel className="mr-14 sm:-ml-4 sm:mr-10">
+          <CarouselContent>
+            {post.media?.map((media, index) => (
+              <CarouselItem key={index} className="sm:basis-1/2 cursor-pointer h-full" onClick={() => openZoomedImage(media)}>
+                <img src={media || ""} alt="post image" className="h-full w-fit rounded" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="hidden group-hover:flex transition-all" />
+          <CarouselNext className="hidden group-hover:flex transition-all" />
+        </Carousel>
+      ) : (
+        post.media && post.media.length === 1 ? (
+          <img src={post.media[0]} alt="post image" className="w-fit rounded cursor-pointer" onClick={() => openZoomedImage(post.media[0])}/>
+        ) : null
+      )}
+
+      {/* Render the zoomed image */}
+      {zoomedImage && (
+        <div className="zoomed-image-container" onClick={closeZoomedImage}>
+          <img src={zoomedImage} alt="zoomed-image" />
+        </div>
+      )}
+
       <div className="flex items-center gap-2">
         <div>
           <LikeButton
