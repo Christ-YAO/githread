856558b90a6bcb72b1d426ece48b50e997cba36c
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-08-30 17:34:12 +0000

    redirect to home page when post not founded

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -2,7 +2,7 @@ import Post from "@/components/features/post/Post";
 import { getAuthSession } from "@/lib/auth";
 import { getPostView } from "@/query/post.query";
 import clsx from "clsx";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function PostView({
   params,
@@ -18,17 +18,17 @@ export default async function PostView({
   const post = await getPostView(params.postId, session?.user.id);
 
   if (!post) {
-    return notFound();
+    return redirect("/");
   }
 
   return (
     <div className="divide-y divide-accent">
-      {post.parent && <Post post={post.parent} key={post.parent.id} />}
+      {post.parent && <Post post={post.parent} key={post.parent.id} userId={session?.user.id} />}
       <div className={clsx({"ml-10": post.parent})}>
-        <Post post={post} key={post.id} />
+        <Post post={post} key={post.id} userId={session?.user.id}/>
         <div className="ml-10 divide-y divide-accent">
             {post.replies.map((reply) => (
-                <Post post={reply} key={reply.id} />
+                <Post post={reply} key={reply.id} userId={session?.user.id}/>
             ))}
         </div>
       </div>
