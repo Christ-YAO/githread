3121cd9e4c7913c7da35fb0aaf7a5ec3ddb8ad92
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2025-02-06 14:44:14 +0000

    correct buffer type error

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,13 +2,14 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { promises as fs } from "fs";
 import { getUser } from "@/query/user.query";
+import path from "path";
 
 export async function POST(request: Request) {
   const formData = await request.formData();
   const content = formData.get("content") as string;
   const media = formData.get("media") as File | null;
 
-  const user = await getUser(); // Assurez-vous que cette fonction est définie pour récupérer l'utilisateur
+  const user = await getUser();
 
   let mediaFileName: string | null = null;
 
@@ -16,7 +17,13 @@ export async function POST(request: Request) {
     const arrayBuffer = await media.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     mediaFileName = `${Date.now()}_${media.name}`;
-    await fs.writeFile(`public/${mediaFileName}`, buffer);
+    const publicDir = path.join(process.cwd(), "public");
+
+    await fs.mkdir(publicDir, { recursive: true });
+    await fs.writeFile(
+      path.join(publicDir, mediaFileName),
+      new Uint8Array(buffer)
+    );
   }
 
   const post = await prisma.post.create({
